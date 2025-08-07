0516de434980db712f808388a18b59775c1c46ec
sarthigupta <workwithsarthi@gmail.com>
2025-08-07 22:30:23 +0530

    again changed

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CommunityNoticeboard from './components/CommunityNoticeboard'
+
+const App = () => {
+  return (
+    <CommunityNoticeboard></CommunityNoticeboard>
+  )
+}
+
+export default App
